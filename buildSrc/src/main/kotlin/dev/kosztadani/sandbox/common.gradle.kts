0f52a7f99708816df13e139781c47a9f88a40e10
Dániel Koszta <daniel.koszta@gmail.com>
2026-03-29 13:10:12 +0200

    Upgrade to Java 25
    Change-Id: I15c1fcffff7891048e5701349e98407927af6e01

--- buildSrc/src/main/kotlin/dev/kosztadani/sandbox/common.gradle.kts
+++ buildSrc/src/main/kotlin/dev/kosztadani/sandbox/common.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
     withSourcesJar()
     withJavadocJar()
